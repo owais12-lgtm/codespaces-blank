eb2add0032fc4be6bd80ac23a01744869ae3409f
Owais <owaisahmad1561@gmail.com>
2026-08-01 21:37:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,8 @@ const gridHelper = new THREE.GridHelper(10, 20, 0x88aaff, 0x445566);
 scene.add(gridHelper);
 
 const textureLoader = new THREE.TextureLoader();
-const texturePath = './owae.png';
+
+const texturePath = 'https://cdn.hackclub.com/019fbe14-05ca-7915-883c-d3c0d4e0ec2e/owae.png';
 
 const cubeTexture = textureLoader.load(
     texturePath,
